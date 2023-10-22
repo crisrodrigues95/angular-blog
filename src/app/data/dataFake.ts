4b61d97ed9512610348b69cbacb23f9d854b4cfb
Cristhian Rodrigues <cristhian.rodrigues95@gmail.com>
2023-10-22 16:22:01 -0300

    entrega

--- src/app/data/dataFake.ts
+++ src/app/data/dataFake.ts
@@ -1,16 +1,30 @@
 export const dataFake = [
   {
-    "id": 1,
-    "photo": "",
-    "title": "",
-    "description": ""
+    "id": "1",
+    "title": "Novo Filme Homem de ferro anunciado",
+    "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec dolor dui, sollicitudin bibendum metus vel, consequat egestas arcu. Praesent sit amet tortor est. In in lacus enim. Cras dictum magna vitae nisl viverra varius. Duis tempor lacus nisl, eu aliquam nisi tempus et. Ut semper vitae magna in feugiat. Duis dolor turpis, rutrum a purus ac, feugiat gravida urna. Praesent ac lectus imperdiet, feugiat diam in, consequat odio.",
+    "photoCover": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ9pdhqug5TVko_cAKou7C3rBed0_kj_AarNA&usqp=CAU"
 
   },
   {
-    "id": 2,
-    "photo": "",
-    "title": "",
-    "description": ""
+    "id": "2",
+    "title": "Novas Séries da Marvel fazem sucesso",
+    "description": "Após os eventos de Vingadores: Ultimato (2019), Wanda Maximoff/Feiticeira Escarlate (Elizabeth Olsen) e Visão (Paul Bettany) se esforçam para levar uma vida normal no subúrbio e esconder seus poderes. Mas a dupla de super-heróis logo começa a suspeitar que nem tudo está tão certo assim.",
+    "photoCover": "https://nosbastidores.com.br/wp-content/uploads/2021/08/Series-Marvel-Disney-Plus-1024x576-1.jpg"
 
-  }
+  },
+  {
+    "id": "3",
+    "title": "Temporada de 30 anos encerra franquia power Rangers",
+    "description": "Quando Lord Zedd retorna mais poderoso do que nunca, a Equipe Cosmic Fury parte para os cosmos para lutar contra o imperador do mal e salvar o universo que conhecemos.",
+    "photoCover":"https://morphinlegacy.com/wp-content/uploads/2023/08/Power-Rangers-Cosmic-Fury-Press-Photo-II.jpg"
+
+  },
+  {
+    "id": "4",
+    "title": "Filme de Wicked estreia no proximo ano",
+    "description": "O enredo apresenta a história de Elphaba, a Bruxa Má do Oeste, e Glinda, a Bruxa Boa do Sul. As duas se conhecem na Universidade Shiz, dirigida por Madame Morrible, e se tornam amigas improváveis. No entanto, elas precisam lutar contra suas diferenças e a rivalidade por um interesse romântico em comum, Fiyero Tiggular.",
+    "photoCover": "https://br.web.img2.acsta.net/r_1280_720/newsv7/16/07/22/13/49/068163.jpg"
+
+  },
 ]
